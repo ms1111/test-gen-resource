33c2bfd4037ba1d14bd5fa3d93c1a2973a7e908d
Michael Smith <msmith@mindbridge.ai>
2018-02-09 23:37:19 -0500

    Reproduce eclipse-wtp issue

---
+++ dependency/src/main/java/test/dependency/SomeClass.java
@@ -0,0 +1,3 @@
+package test.dependency;
+
+public class SomeClass {}
---
+++ webapp/src/main/java/test/webapp/WebApplication.java
@@ -0,0 +1,12 @@
+package test.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
+
